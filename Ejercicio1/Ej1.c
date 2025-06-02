da9b07b4768b0a207fe213831bf0da61487baf53
Santiago Manghi Scheck <112188622+Santiago935@users.noreply.github.com>
2025-06-02 11:39:15 -0300

    Agrego el sistema de platos corregido y fix bugs

--- Ejercicio1/Ej1.c
+++ Ejercicio1/Ej1.c
@@ -47,7 +47,7 @@ int main()
     }
 
     // Inicializo la mesa
-    int datos_iniciales[N] = {2, 4, 6, 8, 10};
+    int datos_iniciales[N] = {3, 10, 6, 2, 8};
     memcpy(mesa->datos, datos_iniciales, sizeof(datos_iniciales));
     memset(mesa->interacciones, 0, sizeof(mesa->interacciones));
     mesa->finalizar = 0;
@@ -61,140 +61,162 @@ int main()
         perror("sem_init");
         exit(EXIT_FAILURE);
     }
-
-    pid_t cheff = fork();
-    // Empieza el codigo
-    if (cheff == 0)
+    // Declaro variable del menu y platos
+    int opc;
+    int platos[3] = {0, 0, 0};
+    int num;
+    // Menu de Platos
+    
+    while(1)
     {
-
-        pid_t cocineroCorta = fork();
-        if (cocineroCorta == 0)
+        printf("\tMenu:\n1- Pastel de Papas.\n2- Guiso de lentejas.\n3- Locro.\n4- Salir.\n");
+        // Espero que me ingrese una opción
+        scanf("%d", &opc);
+        // Valido las opciones de entradas
+        if (opc > 0 && opc < 4)
         {
 
-            sem_wait(semA);
-            sem_wait(semB);
-            sem_wait(semB);
 
-            for (int i = 0; i < N; i++)
-            {
-                mesa->datos[i] = i / 2;
-
-                printf("Cocinero corta ingrediente %d\n", mesa->datos[i]);
+            switch (opc){
+                case 1: num = 2;
+                case 2: num = 5;
+                case 3: num = 8;
             }
-            mesa->interacciones[0]++;
-            sleep(10);
-
-            sem_post(semC);
-            sem_post(semB);
-            exit(0);
-        }
-        pid_t cocineroPica = fork();
-        if (cocineroPica == 0)
-        {
-
-            sem_wait(semB);
-            sem_wait(semD);
-
-            for (int i = 0; i < N; i++)
+            pid_t cheff = fork();
+            platos[opc]++;
+            // Empieza el codigo
+            if (cheff == 0)
             {
-                mesa->datos[i] = i * 2;
-                printf("Cocinero pica ingrediente %d\n", mesa->datos[i]);
-            }
 
-            mesa->interacciones[1]++;
-            sleep(15);
+                pid_t cocineroCorta = fork();
+                if (cocineroCorta == 0)
+                {
 
-            sem_post(semA);
-            sem_post(semA);
-            exit(0);
-        }
+                    sem_wait(semA);
+                    sem_wait(semB);
+                    sem_wait(semB);
 
-        pid_t cocineroCocina = fork();
-        if (cocineroCocina == 0)
-        {
-
-            sem_wait(semC);
-            sem_wait(semC);
-            sem_wait(semB);
-
-            for (int i = 0; i < N; i++)
-            {
-                mesa->datos[i] = i + 2;
-                printf("Cocinero cocina ingrediente %d\n", mesa->datos[i]);
-            }
-            mesa->interacciones[2]++;
-            sleep(10);
-            sem_post(semD);
-            sem_post(semD);
-            exit(0);
-        }
-
-        pid_t cocineroEmpalta = fork();
-        if (cocineroEmpalta == 0)
-        {
-            sem_wait(semD);
-            sem_wait(semA);
-
-            // Simula ordenar ingredientes
-            for (int i = 0; i < N - 1; i++)
-                for (int j = 0; j < N - i - 1; j++)
-                    if (mesa->datos[j] > mesa->datos[j + 1])
+                    for (int i = 0; i < N; i++)
                     {
-                        int tmp = mesa->datos[j];
-                        mesa->datos[j] = mesa->datos[j + 1];
-                        mesa->datos[j + 1] = tmp;
+                        mesa->datos[i] = mesa->datos[i] / num;
+
+                        printf("Cocinero corta ingrediente %d\n", mesa->datos[i]);
+                    }
+                    mesa->interacciones[0]++;
+                    sleep(10);
+
+                    sem_post(semC);
+                    sem_post(semB);
+                    exit(0);
+                }
+                pid_t cocineroPica = fork();
+                if (cocineroPica == 0)
+                {
+
+                    sem_wait(semB);
+                    sem_wait(semD);
+
+                    for (int i = 0; i < N; i++)
+                    {
+                        mesa->datos[i] = mesa->datos[i] * num;
+                        printf("Cocinero pica ingrediente %d\n", mesa->datos[i]);
                     }
 
-            printf("Cocinero empalta ingredientes: ");
+                    mesa->interacciones[1]++;
+                    sleep(15);
 
-            for (int i = 0; i < N; i++)
-            {
+                    sem_post(semA);
+                    sem_post(semA);
+                    exit(0);
+                }
 
-                printf("%d", mesa->datos[i]);
+                pid_t cocineroCocina = fork();
+                if (cocineroCocina == 0)
+                {
+
+                    sem_wait(semC);
+                    sem_wait(semC);
+                    sem_wait(semB);
+
+                    for (int i = 0; i < N; i++)
+                    {
+                        mesa->datos[i] = mesa->datos[i] + num;
+                        printf("Cocinero cocina ingrediente %d\n", mesa->datos[i]);
+                    }
+                    mesa->interacciones[2]++;
+                    sleep(10);
+                    sem_post(semD);
+                    sem_post(semD);
+                    exit(0);
+                }
+
+                pid_t cocineroEmpalta = fork();
+                if (cocineroEmpalta == 0)
+                {
+                    sem_wait(semD);
+                    sem_wait(semA);
+
+                    // Simula ordenar ingredientes
+                    for (int i = 0; i < N - 1; i++)
+                        for (int j = 0; j < N - i - 1; j++)
+                            if (mesa->datos[j] > mesa->datos[j + 1])
+                            {
+                                int tmp = mesa->datos[j];
+                                mesa->datos[j] = mesa->datos[j + 1];
+                                mesa->datos[j + 1] = tmp;
+                            }
+
+                    printf("Cocinero empalta ingredientes: ");
+
+                    for (int i = 0; i < N; i++)
+                    {
+
+                        printf("%d ,", mesa->datos[i]);
+                    }
+
+                    mesa->interacciones[3]++;
+                    sleep(20);
+
+                    sem_post(semB);
+                    sem_post(semC);
+                    exit(0);
+                }
+                // Espera a los 4 cocineros
+                for (int i = 0; i < HIJOS; i++)
+                    wait(NULL);
+                
+                exit(0);
             }
-
-            mesa->interacciones[3]++;
-            sleep(20);
-
-            sem_post(semB);
-            sem_post(semC);
-            exit(0);
+            
+            waitpid(cheff, NULL, 0);
         }
-        // Espera a los 4 cocineros
-        for (int i = 0; i < HIJOS; i++)
+        else if (opc == 4)
         {
-            wait(NULL);
-        }
-        exit(0);
-    }
-    else if (cheff > 0)
-    {
-        wait(NULL);
 
-        printf("\n--- Informe final ---\n");
-        printf("Plato final: ");
-        for (int i = 0; i < N; i++)
-            printf("%d ", mesa->datos[i]);
-        printf("\nInteracciones por cocinero:\n");
-        printf("Cortar: %d\n", mesa->interacciones[0]);
-        printf("Picar: %d\n", mesa->interacciones[1]);
-        printf("Cocinar: %d\n", mesa->interacciones[2]);
-        printf("Emplatar: %d\n", mesa->interacciones[3]);
+                printf("\n--- Informe final ---\n");
+                for (int i = 1; i < 4; i++)
+                    printf("Se pidieron en total del palto %d: %d\n", i, platos[i]);
+                printf("\nInteracciones por cocinero:\n");
+                printf("Cortar: %d\n", mesa->interacciones[0]);
+                printf("Picar: %d\n", mesa->interacciones[1]);
+                printf("Cocinar: %d\n", mesa->interacciones[2]);
+                printf("Emplatar: %d\n", mesa->interacciones[3]);
+            // Liberar recursos
+            sem_destroy(semA);
+            sem_destroy(semB);
+            sem_destroy(semC);
+            sem_destroy(semD);
+            munmap(mesa, sizeof(Mesa));
+            munmap(semA, sizeof(sem_t));
+            munmap(semB, sizeof(sem_t));
+            munmap(semC, sizeof(sem_t));
+            munmap(semD, sizeof(sem_t));
+            break;
+        }
+        else
+            printf("Opcion Invalida. Ingrese Nuevamente..\n");
+
     }
-    else
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    sem_destroy(semA);
-    sem_destroy(semB);
-    sem_destroy(semC);
-    sem_destroy(semD);
-    munmap(mesa, sizeof(Mesa));
-    munmap(semA, sizeof(sem_t));
-    munmap(semB, sizeof(sem_t));
-    munmap(semC, sizeof(sem_t));
-    munmap(semD, sizeof(sem_t));
 
     return 0;
 }
